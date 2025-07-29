8332853369099a468b3262cc3c009f3572e0fff2
aya-othmen7 <ayaothmen7@gmail.com>
2025-07-29 11:50:00 +0100

    add task list

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Task from './Task';
 
 const ListTask = () => {
+   const tasks= useSelector(state=> state.task.tasklist);
+   console.log(tasks);
+   
   return (
-    <div>ListTask</div>
+    <div className='todo-list'>
+        {
+            tasks.map((task)=><Task task={task}/>)
+        }
+    </div>
   )
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Task = () => {
+const Task = ({ task }) => {
   return (
-    <div>Task</div>
-  )
-}
+    <div className={`todo-item ${task.isDone?"done" : "undone"}`}>
+      <div className="text">
+        <h2>{task.title}</h2>
+        <p>{task.description}</p>
+        <span>{task.isDone ? "Done" : "Not Done"}</span>
+      </div>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
