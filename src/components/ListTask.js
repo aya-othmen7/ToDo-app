a697e9e457011094d4a95100f1e83d80bc6db49b
aya-othmen7 <ayaothmen7@gmail.com>
2025-07-29 15:11:59 +0100

    add filter by don/undone

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -2,17 +2,24 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Task from "./Task";
 import AddTask from "./AddTask";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 const ListTask = () => {
   const tasks = useSelector((state) => state.task.tasklist);
   console.log(tasks);
-
+  const [showDone, setshowDone] = useState(false);
   return (
     <div className="todo-list">
       <AddTask />
-      {tasks.map((task) => (
-        <Task task={task} />
-      ))}
+      <Button onClick={() => setshowDone(!showDone)}>
+        {showDone ? "show undone" : "show done"}
+      </Button>
+      {tasks
+        .filter((task) => task.isDone === showDone)
+        .map((task) => (
+          <Task task={task} />
+        ))}
     </div>
   );
 };
